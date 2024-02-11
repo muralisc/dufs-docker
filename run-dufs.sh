ad98dca1ee3457b41756ddc08c2c9878cc7baf2e
Murali Suresh <muralisc@gmail.com>
2024-02-11 16:39:02 +0000

    Separate into file

--- run-dufs.sh
+++ run-dufs.sh
@@ -1,11 +1,9 @@
 
 
-```sh
-password="<>"
+password="$1"
 
 docker run \
   -v /home/murali/data/Recordings:/data/Recordings \
   -v /home/murali/data/Media/notForKids:/data/notforkids \
   -v /home/murali/shared_folders/:/data/shared_folders \
   -p 9001:5000 --rm -d sigoden/dufs /data --allow-search -a "murali:$password@/:rw,/data/Recordings:rw,/data/notforkids:rw"
-```
