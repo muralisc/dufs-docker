4d930963c517f2bc120a419aba7db0a70f89cc34
Murali Suresh <muralisc@gmail.com>
2024-12-29 00:02:40 +0000

    Add tmp dir

--- run-dufs.sh
+++ run-dufs.sh
@@ -6,4 +6,5 @@ docker run \
   -v /home/murali/data/Recordings:/data/Recordings \
   -v /home/murali/data/Media/notForKids:/data/notforkids \
   -v /home/murali/shared_folders/:/data/shared_folders \
+  -v /var/tmp/ledger/:/data/var_tmp_ledger \
   -p 9001:5000 --rm -d sigoden/dufs /data --allow-search -a "murali:$password@/:rw,/data/Recordings:rw,/data/notforkids:rw"
